3b7110bbdb30dc5e2c761925645b6cde815e8d89
sung-eun <sungeunj0@naver.com>
2021-02-02 20:01:48 +0900

    [Feat] Edit main list item layout

--- app/src/main/java/com/sork/sork/main/ProductViewHolder.kt
+++ app/src/main/java/com/sork/sork/main/ProductViewHolder.kt
@@ -5,10 +5,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.sork.common.extension.setOnClickListenerWithHaptic
 import com.sork.domain.entity.ProductSummary
-import com.sork.sork.R
 import com.sork.sork.databinding.ItemMainProductBinding
 
 class ProductViewHolder(private val binding: ItemMainProductBinding, private val onClickItem: (id: String) -> Unit = {}) :
@@ -24,10 +22,6 @@ class ProductViewHolder(private val binding: ItemMainProductBinding, private val
         itemView.setOnClickListenerWithHaptic { item?.let { onClickItem(it.id) } }
     }
 
-    private val radius: Int by lazy {
-        itemView.context.resources.getDimensionPixelSize(R.dimen.main_thumbnail_radius)
-    }
-
     private var item: ProductSummary? = null
 
     fun bind(item: ProductSummary) {
@@ -35,7 +29,7 @@ class ProductViewHolder(private val binding: ItemMainProductBinding, private val
 
         Glide.with(itemView.context)
             .load(item.imageUrl)
-            .transform(CenterCrop(), RoundedCorners(radius))
+            .transform(CenterCrop())
             .into(binding.thumbnail)
 
         binding.brandNameText.text = item.brandName
